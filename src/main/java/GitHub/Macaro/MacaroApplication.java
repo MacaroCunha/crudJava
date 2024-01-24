b159843a3c404dc59e7bc15f047014dc67ffc57b
MacaroCunha <xmariacaroline@outlook.com>
2024-01-23 21:30:24 -0300

    Initial

---
+++ src/main/java/GitHub/Macaro/MacaroApplication.java
@@ -0,0 +1,13 @@
+package GitHub.Macaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GitHub/Macaro/MacaroApplicationTests.java
@@ -0,0 +1,13 @@
+package GitHub.Macaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
